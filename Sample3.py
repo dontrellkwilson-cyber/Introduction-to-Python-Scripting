beddbd4ecc7aee3185c399cab2cb469bb40f8924
Dontrell Wilson <dontrellkwilson.career@gmail.com>
2026-03-30 04:55:26 -0400

    Add files via upload

---
+++ Sample3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+name = "John" # String
+age = 25 # Integer
+height = 5.9 # Float
+is_student = True # Boolean
+numbers = [1, 2, 3, 4] # List
+
+
+print("Name:" , name)
+print("Age:" , age)
+print("Height:" , height)
+print("Student:", is_student)
+print("Numbers:", numbers)
---
+++ Sample4.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+number = int(input("Enter a number: "))
+
+if number < 5:
+	print(number, "is less than 5")
+elif number >= 5:
+	print(number, "is greater than or equal to 5")
+else:
+	print("Error occurred!")
